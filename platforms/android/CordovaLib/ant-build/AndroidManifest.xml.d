6772c5dc4f084f0c91fb44d27bc63184e9f45c80
Liming Xie <liming.xie@gmail.com>
2014-07-08 14:11:01 +0800

    update .gitignore to ingore ant build files

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/liming/workspace/cordova-apps/best2048/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/liming/workspace/cordova-apps/best2048/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/liming/workspace/cordova-apps/best2048/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/liming/workspace/cordova-apps/best2048/platforms/android/AndroidManifest.xml \
